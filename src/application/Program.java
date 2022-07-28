18a3e808d21d2ef091b58eea8b05061118b465e5
Moises Eduardo <moises.efsantos@gmail.com>
2022-07-28 00:24:24 -0300

    Added bom dia message

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,16 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite bom dia: ");
+        String bomDia = sc.nextLine();
+
+        sc.close();
     }
 }
